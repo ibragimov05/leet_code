b0fee6ed79f0e82b9c6fb59ac5b81bdc2c199bd3
Fazliddin <fazliddin@Fazliddins-MacBook-Pro.local>
2024-11-10 14:41:30 +0500

    LeetCode

---
+++ python/35.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        return 1
+
+
+s = Solution()
+print(s.searchInsert([1, 3, 5, 6], 5))
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
