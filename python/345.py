7086685fde6167395a18b51114d09e817999c35f
fazliddin <ibragimov.asus15@gmail.com>
2024-11-13 19:49:51 +0500

    LeetCode

--- python/345.py
+++ python/345.py
@@ -16,17 +16,3 @@ class Solution:
                 right -= 1
                 
         return "".join(chars)
-
-
-s = Solution()
-print(s.reverseVowels("IceCreAm"))
-
-# Example 1:
-# Input: s = "IceCreAm"
-# Output: "AceCreIm"
-# Explanation:
-# The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
-
-# Example 2:
-# Input: s = "leetcode"
-# Output: "leotcede"
--- python/535.py
+++ python/535.py
@@ -7,7 +7,3 @@ class Codec:
     def decode(self, shortUrl: str) -> str:
         return shortUrl
         
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
