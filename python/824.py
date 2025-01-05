dbacdd81fae16ec749cd7997359a7b0aee123f37
fazliddin <ibragimov.asus15@gmail.com>
2025-01-05 21:01:16 +0500

    LeetCode

---
+++ python/824.py
@@ -0,0 +1,13 @@
+class Solution:
+	def toGoatLatin(self, sentence: str) -> str:
+		vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+		words = sentence.split()
+
+		got_latin_words = [
+			(
+				word + "ma" if word[0] in vowels else word[1:] + word[0] + "ma"
+			) + "a" * (i + 1)
+			for i, word in enumerate(words)
+		]
+
+		return " ".join(got_latin_words)
