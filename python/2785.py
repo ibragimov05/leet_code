6664bc3cf277caaa970c840913dfe62f9dad071d
fazliddin <ibragimov.asus15@gmail.com>
2025-01-06 20:05:15 +0500

    LeetCode

---
+++ python/2785.py
@@ -0,0 +1,15 @@
+class Solution:
+	def sortVowels(self, s: str) -> str:
+		vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+		vowels_ls = []
+
+		for i in s:
+			if i in vowels:
+				vowels_ls.append(i)
+		print(vowels_ls)
+		return ''
+
+
+
+print(Solution().sortVowels("lEetcOde"))  # lEOtcede
+print(Solution().sortVowels("lYmpH"))  # lYmpH
