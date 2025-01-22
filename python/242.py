a66b597212693dfeb33a68a54daa77207f06a7dc
fazliddin <ibragimov.asus15@gmail.com>
2025-01-22 20:33:14 +0500

    LeetCode

--- python/242.py
+++ python/242.py
@@ -1,23 +1,3 @@
 class Solution:
 	def isAnagram(self, s: str, t: str) -> bool:
-		if len(s) != len(t): return False
-
-		for i, j in t, s:
-			if s.count(i) != t.count(j): return False
-
-		return True
-
-
-print(Solution().isAnagram(s="anagram", t="nagaram"))
-
-# Example 1:
-#
-# Input: s = "anagram", t = "nagaram"
-#
-# Output: true
-#
-# Example 2:
-#
-# Input: s = "rat", t = "car"
-#
-# Output: false
+		return sorted(s) == sorted(t)
