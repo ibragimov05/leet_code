659ec42e5ec7972c8f2bb22b1470757d70b2947c
fazliddin <ibragimov.asus15@gmail.com>
2025-01-27 08:36:19 +0500

    LeetCode

---
+++ python/541.py
@@ -0,0 +1,13 @@
+class Solution:
+	def reverseStr(self, s: str, k: int) -> str:
+		return s[::k]
+
+print(Solution().reverseStr("abcdefg", 2))
+# Example 1:
+#
+# Input: s = "abcdefg", k = 2
+# Output: "bacdfeg"
+# Example 2:
+#
+# Input: s = "abcd", k = 2
+# Output: "bacd"
