22ac36f2ebfdda87e2497e6129684ba9600180a8
fazliddin <ibragimov.asus15@gmail.com>
2025-01-27 21:48:45 +0500

    LeetCode

--- python/804.py
+++ python/804.py
@@ -9,31 +9,7 @@ class Solution:
 			"z": "--.."
 		}
 
-		letter_to_morse = []
-		for word in words:
-			res_word = ""
-			for letter in word:
-				res_word += letter_to_morse_alphabet[letter]
-			letter_to_morse.append(res_word)
-
-		print(letter_to_morse)
-		return 1
-
-
-print(Solution().uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
-print(Solution().uniqueMorseRepresentations(["a"]))
-
-# Example 1:
-#
-# Input: words = ["gin","zen","gig","msg"]
-# Output: 2
-# Explanation: The transformation of each word is:
-# "gin" -> "--...-."
-# "zen" -> "--...-."
-# "gig" -> "--...--."
-# "msg" -> "--...--."
-# There are 2 different transformations: "--...-." and "--...--.".
-# Example 2:
-#
-# Input: words = ["a"]
-# Output: 1
+		return len({
+			"".join(letter_to_morse_alphabet[letter] for letter in word)
+			for word in words
+		})
