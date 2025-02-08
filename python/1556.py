4cc776c4ad92d3742fc3e6a5ce39bc32e98af29f
fazliddin <ibragimov.asus15@gmail.com>
2025-02-08 08:55:17 +0500

    LeetCode

---
+++ python/1556.py
@@ -0,0 +1,10 @@
+class Solution:
+	def thousandSeparator(self, n: int) -> str:
+		str_n = str(n)
+
+		parts = []
+		for i in range(len(str_n), 0, -3):
+			start = max(0, i - 3)
+			parts.append(str_n[start:i])
+
+		return ".".join(reversed(parts))
