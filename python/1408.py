07cfd02924336487e021c86eb93490f979c20a38
fazliddin <ibragimov.asus15@gmail.com>
2025-02-09 17:07:45 +0500

    LeetCode

--- python/1408.py
+++ python/1408.py
@@ -1,30 +1,10 @@
 class Solution:
 	def stringMatching(self, words: list[str]) -> list[str]:
-		result = []
-
-		for word in words:
-			if word in words:
-				result.append(word)
-
-		return result
-
-print(Solution().stringMatching(["mass","as","hero","superhero"]))
-print(Solution().stringMatching(["leetcode","et","code"]))
-print(Solution().stringMatching( ["blue","green","bu"]))
-
-# Example 1:
-#
-# Input: words = ["mass","as","hero","superhero"]
-# Output: ["as","hero"]
-# Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
-# ["hero","as"] is also a valid answer.
-# Example 2:
-#
-# Input: words = ["leetcode","et","code"]
-# Output: ["et","code"]
-# Explanation: "et", "code" are substring of "leetcode".
-# Example 3:
-#
-# Input: words = ["blue","green","bu"]
-# Output: []
-# Explanation: No string of words is substring of another string.
+		return list(
+			set(
+				words[i]
+				for i in range(len(words))
+				for j in range(len(words))
+				if words[i] != words[j] and words[i] in words[j]
+			)
+		)
