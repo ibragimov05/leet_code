efa1d132a8ba8fab4f1f98bb86d7d43dc8ba9129
fazliddin <ibragimov.asus15@gmail.com>
2025-02-22 14:19:13 +0500

    LeetCode

---
+++ c/2011.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int finalValueAfterOperations(char **operations, int operationsSize)
+{
+  int x = 0;
+  for (int i = 0; i < operationsSize; i++)
+  {
+    if (operations[i][1] == '+')
+      ++x;
+    else
+      --x;
+  }
+  return x;
+}
