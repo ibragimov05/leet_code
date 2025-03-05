30872ddd5b5b5a23fe14982b4787aa09d0e2e6be
fazliddin <ibragimov.asus15@gmail.com>
2025-03-05 20:45:28 +0500

    LeetCode

---
+++ c/2148.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int countElements(int *nums, int numsSize)
+{
+  // Bubble sort (ascending order)
+  for (int i = 0; i < numsSize - 1; i++)
+  {
+    for (int j = 0; j < numsSize - i - 1; j++)
+    {
+      if (nums[j] > nums[j + 1])
+      {
+        int temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+
+  int min_num_count = 0;
+  int max_num_count = 0;
+
+  for (int i = 0; i < numsSize; i++)
+  {
+    if (nums[i] == nums[0])
+    {
+      min_num_count++;
+    }
+
+    if (nums[i] == nums[numsSize - 1])
+    {
+      max_num_count++;
+    }
+  }
+
+  int result = numsSize - max_num_count - min_num_count;
+  return result > 0 ? result : 0;
+}
