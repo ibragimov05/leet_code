501b0752fdb7b3226f0f4a7076fdef9f3f57f674
fazliddin <ibragimov.asus15@gmail.com>
2025-03-11 06:38:11 +0500

    LeetCode

--- python/2108.py
+++ python/2108.py
@@ -3,7 +3,4 @@ from typing import List
 
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        for word in words:
-            if word == word[::-1]:
-                return word
-        return ""
+        return next((word for word in words if word == word[::-1]), "")
