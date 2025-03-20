887f2d1bf71b240c3c12ea0c5409fe6a4909091e
fazliddin <ibragimov.asus15@gmail.com>
2025-03-20 05:59:59 +0500

    LeetCode

--- python/33.py
+++ python/33.py
@@ -5,5 +5,5 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         try:
             return nums.index(target)
-        except ValueError as e:
+        except ValueError as _:
             return -1
