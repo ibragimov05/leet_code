fcc411c05a28f0bfbc7c5efc4ca328f7ca9c7f15
fazliddin <ibragimov.asus15@gmail.com>
2025-04-27 11:03:23 +0500

    LeetCode

--- python/1.py
+++ python/1.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
