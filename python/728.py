3e5a2a3d9370a06c44766322f6951747dec0ac27
fazliddin <ibragimov.asus15@gmail.com>
2025-04-28 21:24:46 +0500

    LeetCode

--- python/728.py
+++ python/728.py
@@ -3,17 +3,10 @@ from typing import List
 
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
-        numbers: List[int] = []
+        def is_self_dividing(num: int) -> bool:
+            for digit in str(num):
+                if digit == "0" or num % int(digit) != 0:
+                    return False
+            return True
 
-        for i in range(left, right + 1):
-            is_self_dividing_digit = True
-
-            for char in str(i):
-                int_char: int = int(char)
-                if not (int_char != 0 and i % int_char == 0):
-                    is_self_dividing_digit = False
-                    break
-            if is_self_dividing_digit:
-                numbers.append(i)
-
-        return numbers
+        return [num for num in range(left, right + 1) if is_self_dividing(num)]
