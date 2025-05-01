5dc3ddbc1c1a4a2349892f1e4d66a27824bc8451
fazliddin <ibragimov.asus15@gmail.com>
2025-05-01 18:55:07 +0500

    LeetCode

--- python/ss.py
+++ python/ss.py
@@ -1,6 +1,6 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        alphabet = {}
+        alphabet: dict[str, str] = {}
         i = 0
         for c in key:
             if c != " " and c not in alphabet:
@@ -10,17 +10,9 @@ class Solution:
         result: list[str] = []
 
         for c in message:
-            if c == ' ':
-                result.append(' ')
+            if c == " ":
+                result.append(" ")
             else:
                 result.append(alphabet[c])
 
         return "".join(result)
-
-
-print(Solution().decodeMessage("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb"))
-
-# Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
-# Output: "the five boxing wizards jump quickly"
-# Explanation: The diagram above shows the substitution table.
-# It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
